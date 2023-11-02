3358d2959012cc1b10bb06b8fe3eb74150fd0784
FULLSTACK <alber1897@gmail.com>
2023-11-02 12:23:17 +0100

    c

--- proyectojavascript/js/13-eventos.js
+++ proyectojavascript/js/13-eventos.js
@@ -52,7 +52,7 @@ mensaje.addEventListener("input",evento=>{
 })
 
 function act(){
-    document.querySelector(".parrafo").innerHTML=`Esta es la web de ${nombreF}, con correo
+    document.zquerySelector(".parrafo").innerHTML=`Esta es la web de ${nombreF}, con correo
     ${correoF} y su propuesta es ${mensajeF}`;
 }
    
@@ -67,5 +67,82 @@ function pasar(evento){
     datos[evento.target.id]=evento.target.value;
     console.log(datos);
 }
+function crearDiv(){
+    const seccionZonas1=document.querySelector(".zonas");
+
+    const divGeneral=document.createElement("DIV");
+    divGeneral.classList.add("divGeneral");
+    seccionZonas1.appendChild(divGeneral);
+    
+    const divG=document.querySelector(".divGeneral");
+    
+    const p1=document.createElement("P");
+    p1.classList.add("p1");
+    divG.appendChild(p1);
+
+    const p2=document.createElement("P");
+    p2.classList.add("p2");
+    divG.appendChild(p2);
+
+    const enlaces=document.createElement("DIV");
+    enlaces.classList.add("enlaces");
+    divG.appendChild(enlaces);
+    
+    const seccionEnlaces=document.querySelector(".enlaces")
+
+    const enl1=document.createElement("A");
+    enl1.classList.add("enl1");
+    seccionEnlaces.appendChild(enl1);
+
+    const enl2=document.createElement("A");
+    enl2.classList.add("enl2");
+    seccionEnlaces.appendChild(enl2);
+}
 
 
+function obtenerZonas(){
+     fetch("zonas.json").then(
+        resultado=>{
+            return resultado.json();
+        }
+        ).then(datos=>{
+            const{zonas}=datos;
+            zonas.forEach(element => {
+                
+                const seccionZonas1=document.querySelector(".zonas");
+
+                const divGeneral=document.createElement("DIV");
+                divGeneral.classList.add("divGeneral");
+                seccionZonas1.appendChild(divGeneral);
+                
+                const p1=document.createElement("P");
+                p1.textContent=element.zonas;
+                divGeneral.appendChild(p1);
+            
+                const p2=document.createElement("P");
+                p2.textContent=element.ciudades;
+                divGeneral.appendChild(p2);
+            
+                const enlaces=document.createElement("DIV");
+                enlaces.classList.add("enlaces");
+                divGeneral.appendChild(enlaces);
+                
+                const enl1=document.createElement("A");
+                enl1.classList.add("enl1");
+                enl1.href="aa";
+                enl1.textContent=element.enlaceZonas;
+                enlaces.appendChild(enl1);
+            
+                const enl2=document.createElement("A");
+                enl2.classList.add("enl2");
+                enl2.textContent=element.enlaceCursos;
+                enl2.href="aa";
+                enlaces.appendChild(enl2);
+                
+            });
+        }
+
+        )
+     
+}
+obtenerZonas();
